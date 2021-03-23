19123230b618c960db614690fcafc20b7a1801d4
yujiro-hirashima <75685573+yujiro-hirashima@users.noreply.github.com>
2021-03-24 00:24:46 +0900

    AddLoginDisplay ログイン画面実装

--- src/component/pages/Login.tsx
+++ src/component/pages/Login.tsx
@@ -1,5 +1,38 @@
-import { memo, VFC } from "react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+import { ChangeEvent, memo, useState, VFC } from "react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { UseAuth } from "../../hooks/UseAuth";
 
 export const Login: VFC = memo(() => {
-  return <p>ログインページです</p>;
+  const { login, loading } = UseAuth();
+  const [userId, setUserId] = useState("");
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
+
+  const onClickLogin = () => login(userId);
+
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="xl" shadow="md">
+        <Heading as="h1" size="xl" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input
+            placeholder="ユーザーID"
+            value={userId}
+            onChange={onChangeUserId}
+          />
+          <PrimaryButton
+            disabled={userId === ""}
+            loading={loading}
+            onClick={onClickLogin}
+          >
+            ログイン
+          </PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
---
+++ src/hooks/UseAuth.tsx
@@ -0,0 +1,29 @@
+import { useCallback, useState } from "react";
+import axios from "axios";
+import { User } from "../types/api/user";
+import { useHistory } from "react-router-dom";
+
+export const UseAuth = () => {
+  const history = useHistory();
+  const [loading, setLoading] = useState(false);
+
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
+
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            history.push("/home");
+          } else {
+            alert("ユーザーが見つかりません");
+          }
+        })
+        .catch(() => alert("ログインできません"))
+        .finally(() => setLoading(false));
+    },
+    [history]
+  );
+  return { login, loading };
+};
